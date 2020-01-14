41cd47518c853096fedeb3451cabfe73da243a8b
Max Stevenson <maxstevenson@msn.com>
2020-01-14 21:25:27 +0000

    More setState experimentation.

--- src/index.js
+++ src/index.js
@@ -2,22 +2,36 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-const App = () => {
-  const [count, setCount] = useState(1);
+const App = (props) => {
+  const [count, setCount] = useState(props.count);
 
   const increment = () => {
     setCount(count + 1);
   };
 
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(props.count);
+  };
+
   return (
     <div>
     <button onClick={increment}>+1</button>
+    <button onClick={decrement}>-1</button>
+    <button onClick={reset}>reset</button>
       <p>Count is: {count}</p>
     </div>
   )
 }
 
-ReactDOM.render(<App></App>, document.getElementById('root'));
+App.defaultProps = {
+  count: 0
+};
+
+ReactDOM.render(<App count={5}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
