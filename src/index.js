22fe3d557c3076cd9b7e5eeae6f0f2cbdc9a5c8e
Max Stevenson <maxstevenson@msn.com>
2020-01-17 10:41:02 +0000

    Can add and remove notes.

--- src/index.js
+++ src/index.js
@@ -7,13 +7,32 @@ const NoteApp = () => {
   const [notes, setNotes] = useState([]);
   const [title, setTitle] = useState('');
 
+  const addNote = (event) => {
+    event.preventDefault();
+    setNotes([
+      ...notes,
+      { title }
+    ]);
+    setTitle('');
+  };
+
+  const removeNote = (title) => {
+    setNotes(notes.filter((note) => note.title !== title));
+  };
+
   return (
     <div>
       <h1>Notes</h1>
+      {notes.map((note) => (
+        <div key={note.title}>
+          <h3>{note.title}</h3>
+          <button onClick={() => removeNote(note.title)}>X</button>
+        </div>
+      ))}
       <p>Add Note</p>
-      <p>{title}</p>
-      <form>
+      <form onSubmit={addNote}>
         <input value={title} onChange={(e) => {setTitle(e.target.value)}}></input>
+        <button>add Note</button>
       </form>
     </div>
   )
