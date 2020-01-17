0cc350d03684967c4b1f7708abe1c6a4f1a2070a
Max Stevenson <maxstevenson@msn.com>
2020-01-17 19:35:38 +0000

    Experimenting with custom hooks.

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,14 +1,36 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import NotesContext from '../context/notes-context';
 
-const Note = ({ note }) => {
+const useMousePosition = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setPosition({
+        x: event.pageX,
+        y: event.pageY
+      });
+    };
+
+    document.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+
+  });
+  return position;
+};
+
+const Note = ({ note }) => {
   const { notesDispatch } = useContext(NotesContext);
+  const mousePosition = useMousePosition();
 
   return (
     <div key={note.title}>
       <h3>{note.title}</h3>
       <p>{note.body}</p>
+      <p>{mousePosition.x}, {mousePosition.y}</p>
       <button onClick={() => notesDispatch({ type: 'REMOVE_NOTE', title: note.title })}>X</button>
     </div>
   );
